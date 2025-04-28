565d24266a3a58c260582de2081162815aa101ec
Rahul <rajrahul00945@gmail.com>
2025-04-29 01:04:43 +0530

    Video demo added

--- MovieMate/ViewController.swift
+++ MovieMate/ViewController.swift
@@ -1,4 +1,6 @@
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -7,6 +9,20 @@ class ViewController: UIViewController {
     
     }
 
+    var videoPlayer : AVPlayer!
+    
+    @IBAction func userGuideBtn(_ sender: Any) {
+        let path = Bundle.main.path(forResource: "demo", ofType: "mp4")
+        let urlObj = URL(fileURLWithPath: path!)
+        
+        let videoPlayerController = AVPlayerViewController()
+        videoPlayerController.player = AVPlayer(url: urlObj)
+        
+        present(videoPlayerController, animated: true) {
+            videoPlayerController.player?.play()
+        }
+    }
+    
     
     @IBAction func getStartBtn(_ sender: Any) {
         performSegue(withIdentifier:"toResiter" , sender: nil)
